0a52c5f7014bf99b9f45f443954c4357b21bbcd1
Artem <artem-shklyarik@yandex.ru>
2016-08-21 19:16:17 +0300

    Index View

--- laravel/resources/views/main/index.blade.php
+++ laravel/resources/views/main/index.blade.php
@@ -1,47 +1,6 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
-
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Unnamed Social Network</div>
-                creating by
-                <a href="https://www.linkedin.com/in/artem-shklyarik-bb2b14120" target="_blank">Artem Shklyarik</a>
-            </div>
-        </div>
-    </body>
-</html>
+@extends('layout.app')
+@section('content')
+    <div class="content">
+        Main Page
+    </div>
+@endsection
